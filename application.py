3aa3ac9a8bb7d84f6b639663366f3a69721a111a
Theodore Liu <theodoretliu@gmail.com>
2016-11-23 23:41:50 -0500

    fixed port

--- application.py
+++ application.py
@@ -1,11 +1,8 @@
 from flask import Flask, flash, redirect, render_template, request, session, url_for
-from flask_session import Session
 
-    
+
 # configure application
 app = Flask(__name__)
-
-Session(app)
 
 @app.route("/")
 def index():
@@ -19,3 +16,7 @@ def about():
 def parse():
 	code = request.args.get("input")
 	return code
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
