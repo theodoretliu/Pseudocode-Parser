367517ddaf74f28aec2d519f929b586c62570e9b
Theodore Liu <theodoretliu@gmail.com>
2016-12-01 11:13:47 -0500

    really fixed multiple users. Allows for up to 10000 users.

--- application.py
+++ application.py
@@ -3,17 +3,26 @@ from flask import Flask, flash, redirect, render_template, request, session, url
 from power import *
 from parser import *
 
-
-# configure application
-app = Flask(__name__)
-
 roots = dict()
 currents = dict()
+counter = 0
+# configure application
+app = Flask(__name__)
+app.secret_key = b"}\xa8\x1b\xa6/\xf4\x91\xc3\xb2cQ\xa5Ec\x08\x13B$\x0f\x87\x07a\xec\xea"
 
 @app.route("/")
 def index():
-	roots[request.environ['REMOTE_ADDR']] = Parent()
-	currents[request.environ['REMOTE_ADDR']] = roots[request.environ['REMOTE_ADDR']]
+	global counter
+
+	if counter > 10000:
+		counter = 0
+
+	session["id"] = counter
+	app.logger.debug(session["id"])
+	counter += 1
+
+	roots[session["id"]] = Parent()
+	currents[session["id"]] = roots[session["id"]]
 
 	return render_template("index.html")
 
@@ -24,9 +33,9 @@ def about():
 @app.route("/parse")
 def parse():
 	pseudocode = request.args.get("input")
-	currents[request.environ['REMOTE_ADDR']] = parse_input(pseudocode, currents[request.environ['REMOTE_ADDR']])
+	currents[session["id"]] = parse_input(pseudocode, currents[session["id"]])
 
-	return str(roots[request.environ['REMOTE_ADDR']])
+	return str(roots[session["id"]])
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
