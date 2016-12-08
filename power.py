fe13d29ec6187c5455c65de228cb122c1e278225
Theodore Liu <theodoretliu@gmail.com>
2016-12-08 03:48:23 -0500

    added comments to power.py

--- power.py
+++ power.py
@@ -1,4 +1,6 @@
+# the grandaddy of all the classes. other classes will inherit from this class
 class Parent():
+    # constructor. set args = None to avoid a weird Python thing that I won't go into
     def __init__(self, args=None, level=-1):
         if args is None:
             self.args = []
@@ -15,6 +17,8 @@ class Parent():
             arg.set_level(self.level + 1)
             arg.set_parent(self)
 
+    # override the default string function. Instead iterate over the arguments and return their
+    # string values
     def __str__(self):
         ret = ""
 
@@ -23,6 +27,8 @@ class Parent():
 
         return ret
 
+    # adds an argument as a child to the object. sets the objects level and parent attributes
+    # accordingly
     def add_arg(self, arg):
         if isinstance(arg, str):
             self.args.append(arg)
@@ -32,6 +38,8 @@ class Parent():
         arg.set_parent(self)
         self.args.append(arg)
 
+    # decrease the level of the object (indentation) and decrease the indentation of every one of
+    # its children as well
     def decrement_level(self):
         if self.level == 0:
             return
@@ -40,6 +48,7 @@ class Parent():
         for arg in self.args:
             arg.decrement_level()
 
+    # bunch of getters
     def get_args(self):
         return self.args
 
@@ -54,9 +63,11 @@ class Parent():
         for arg in self.args:
             arg.increment_level()
 
+    # removes the last child of the object
     def remove_arg(self):
         self.args.pop(-1)
 
+    # sets the level of the object to whatever desired
     def set_level(self, level):
         if level < 0:
             return
@@ -68,10 +79,14 @@ class Parent():
             while self.level > level:
                 self.decrement_level()
 
+    # changes the parent attribute of the current object
     def set_parent(self, parent):
         self.parent = parent
 
+# class for Print. inherits from the Parent class
 class Print(Parent):
+    # constructor for Print does not use the super constructor from Parent class because
+    # the children of print don't really have "levels" to be set
     def __init__(self, args=None, level=0):
         if args is None:
             self.args = []
@@ -83,6 +98,7 @@ class Print(Parent):
         if not isinstance(self.args, list):
             self.args = [self.args]
 
+    # returns the proper Python code for a print with proper indentation
     def __str__(self):
         ret = " " * 4 * self.level + "print("
 
@@ -92,10 +108,15 @@ class Print(Parent):
 
         return ret
 
+    # overrides the set_level from Parent again because the children of Print can't have their levels
+    # set
     def set_level(self, level):
         self.level = level
 
+# class for a For Loop
 class ForLoop(Parent):
+    # constructor with lower, upper, iterating variable, args, and level
+    # uses the super constructor
     def __init__(self, lower=0, upper=10, var="i", args=None, level=0):
         super().__init__(args, level)
 
@@ -103,6 +124,7 @@ class ForLoop(Parent):
         self.upper = upper
         self.var = var
 
+    # Python code for a loop
     def __str__(self):
         if len(self.args) == 0:
             return " " * 4 * self.level + "for {} in range({}{}):\n".format(
@@ -116,12 +138,14 @@ class ForLoop(Parent):
 
         return ret
 
+    # getters
     def get_lower(self):
         return self.lower
 
     def get_upper(self):
         return self.upper
 
+    # setters
     def set_bounds(self, lower, upper):
         self.lower = lower
         self.upper = upper
@@ -132,6 +156,7 @@ class ForLoop(Parent):
     def set_upper(self, upper):
         self.upper = upper
 
+# class for While Loop
 class WhileLoop(Parent):
     def __init__(self, condition="", args=None, level=0):
         super().__init__(args, level)
@@ -149,6 +174,7 @@ class WhileLoop(Parent):
 
         return retval
 
+# class for If object
 class If(Parent):
     def __init__(self, condition="", args=None, level=0):
         super().__init__(args, level)
@@ -254,6 +280,8 @@ class Class(Parent):
     def get_name(self):
         return self.name
 
+# Generic class in case we want to put some raw strings inside the code without needing to go
+# through the hassle of creating an object
 class Generic(Parent):
     def __init__(self, args=None, level=0):
         if args is None:
@@ -293,6 +321,7 @@ class Return(Parent):
     def set_level(self, level):
         self.level = level
 
+# testing
 if __name__ == "__main__":
     from parser import *
 
